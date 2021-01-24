6455c46b2c41127788b3b3ddac9e0e906a41de39
expandery <peter@expandery.com>
2021-01-24 15:09:39 +0200

    test

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -19,7 +19,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users.ToListAsync();d
         }
 
         [HttpGet("{id}")]
